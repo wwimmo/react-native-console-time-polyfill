a1e078b24a5e638c966b57738baaf3de34f6ae3d
Thomas Steinbrüchel <tst@wwimmo.ch>
2021-01-26 18:29:22 +0100

    Try to add return value on timeEnd

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
-'use strict'
+"use strict";
 
 const PerformanceNow =
   global.performanceNow ||
-  global.nativePerformanceNow  ||
-  require('fbjs/lib/performanceNow');
+  global.nativePerformanceNow ||
+  require("fbjs/lib/performanceNow");
 
-const DEFAULT_LABEL = 'default';
-const DEFAULT_PREC  = 3;
+const DEFAULT_LABEL = "default";
+const DEFAULT_PREC = 3;
 
-let counts     = {};
+let counts = {};
 let startTimes = {};
 
-console.time    = console.time    || ((label = DEFAULT_LABEL) => { startTimes[label] = PerformanceNow() });
-console.timeLog = console.timeLog || ((label = DEFAULT_LABEL, desc) => timeRecord(label, desc));
-console.timeEnd = console.timeEnd || ((label = DEFAULT_LABEL) => timeRecord(label, undefined, true));
+console.time = (label = DEFAULT_LABEL) => {
+  startTimes[label] = PerformanceNow();
+};
+console.timeLog = (label = DEFAULT_LABEL, desc) => timeRecord(label, desc);
 
-console.count = console.count || ((label = DEFAULT_LABEL) => {
+console.count = (label = DEFAULT_LABEL) => {
   if (!counts[label]) {
     counts[label] = 0;
   }
   counts[label]++;
   console.log(`${label}: ${counts[label]}`);
-});
+};
 
-console.countReset = console.countReset || ((label = DEFAULT_LABEL) => {
+console.countReset = (label = DEFAULT_LABEL) => {
   if (counts[label]) {
     counts[label] = 0;
   } else {
     console.warn(`Count for '${label}' does not exist`);
   }
-});
+};
+
+export function timeEnd(label = DEFAULT_LABEL) {
+  return timeRecord(label, undefined, true);
+}
 
 function timeRecord(label, desc, final) {
   const endTime = PerformanceNow();
   const startTime = startTimes[label];
+  let returnValue = "";
   if (startTime) {
     const delta = endTime - startTime;
     if (desc) {
-      console.log(`${label}: ${delta.toFixed(DEFAULT_PREC)}ms ${desc}`);
+      returnValue = `${label}: ${delta.toFixed(DEFAULT_PREC)}ms ${desc}`;
     } else {
-      console.log(`${label}: ${delta.toFixed(DEFAULT_PREC)}ms`);
+      returnValue = `${label}: ${delta.toFixed(DEFAULT_PREC)}ms`;
     }
     if (final) delete startTimes[label];
+    return returnValue;
   } else {
     console.warn(`Timer '${label}' does not exist`);
+    return returnValue;
   }
 }
